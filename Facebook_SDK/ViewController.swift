7390e115e01115eaf3dfa4c2bced801174d37ea4
AnaMepisashvili <85555767+AnaMepisashvili@users.noreply.github.com>
2022-01-13 18:19:34 +0400

    Initial Commit

---
+++ Facebook_SDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Facebook_SDK
+//
+//  Created by Ana Mepisashvili on 13.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
